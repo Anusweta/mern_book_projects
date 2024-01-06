12f7f79f7286e93f2f560353a02651cc4e14fa57
Anusweta <sasmita.manmita@gmail.com>
2024-01-06 10:42:26 +0530

    commit

--- mern-client/src/main.jsx
+++ mern-client/src/main.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './routers/router.jsx'
-import AuthProvider from './contects/AuthProvider.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
+import { RouterProvider } from 'react-router-dom';
+import router from './routers/router.jsx';
+import AuthProvider from './contects/AuthProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <AuthProvider>
-    <RouterProvider router={router} />
-    </AuthProvider>
+    <Router> {/* Wrap your entire application in BrowserRouter */}
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </Router>
   </React.StrictMode>,
-)
+);
